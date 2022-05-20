d67fe56776bbf26701ea70fce33b9d6f8beba6c8
Luke Orriss <73408132+lukeorriss@users.noreply.github.com>
2022-05-20 11:35:24 +0100

    Update button_lizard.py

--- button_lizard.py
+++ button_lizard.py
@@ -24,30 +24,38 @@ def timeLeft(seconds):
     time_left = interval - seconds
     return str(datetime.timedelta(seconds=time_left))
 
+def retry():
+    main()
 def main():
-    while True:
-        input_sate = GPIO.input(5)
+    try:
+        while True:
+            input_sate = GPIO.input(5)
 
-        if input_sate == False:
+            if input_sate == False:
 
-            GPIO.output(pump, GPIO.HIGH)
-            print("Watering")
-            # lcd.setRGB(60,248,248);
-            lcd.setRGB(255,0,0)
-            lcd.setCursor(0, 0)
-            lcd.printout("Watering...                 ")
-            time.sleep(8)
+                GPIO.output(pump, GPIO.HIGH)
+                print("Watering")
+                # lcd.setRGB(60,248,248);
+                lcd.setRGB(255,0,0)
+                lcd.setCursor(0, 0)
+                lcd.printout("Watering...                 ")
+                time.sleep(8)
 
-            GPIO.output(pump, GPIO.LOW)
+                GPIO.output(pump, GPIO.LOW)
 
-            now = datetime.datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            # Print to Display
-            # lcd.setRGB(248,248,60)
-            lcd.setCursor(0, 0)
-            lcd.printout("Last Watered:                ")
-            lcd.setCursor(0, 1)
-            lcd.printout(dt_string + "                 ")
+                now = datetime.datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                # Print to Display
+                # lcd.setRGB(248,248,60)
+                lcd.setCursor(0, 0)
+                lcd.printout("Last Watered:                ")
+                lcd.setCursor(0, 1)
+                lcd.printout(dt_string + "                 ")
+    except OSError as e:
+        lcd.setRGB(255,0,0)
+        lcd.setCursor(0, 0)
+        lcd.printout(e)
+        retry();
 main()
 
 
