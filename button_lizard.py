93d0fc7c4ab44ce68e5b7113aaa148351bcde30d
Luke Orriss <73408132+lukeorriss@users.noreply.github.com>
2022-05-20 11:06:14 +0100

    Create button_lizard.py

---
+++ button_lizard.py
@@ -0,0 +1,55 @@
+import time
+import os
+import RGB1602
+import RPi.GPIO as GPIO
+import datetime
+
+# Initialise Screen
+lcd = RGB1602.RGB1602(16,2)
+
+# Initialise Pump
+pump = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pump, GPIO.OUT)
+
+# Initialise Button
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Main Routine
+def timeElapsed(seconds):
+    return str(datetime.timedelta(seconds=seconds))
+def timeLeft(seconds):
+    interval = 43200
+    time_left = interval - seconds
+    return str(datetime.timedelta(seconds=time_left))
+
+def main():
+    while True:
+        input_sate = GPIO.input(18)
+
+        if input_sate == False:
+
+            GPIO.output(pump, GPIO.HIGH)
+            print("Watering")
+            # lcd.setRGB(60,248,248);
+            lcd.setRGB(255,0,0)
+            lcd.setCursor(0, 0)
+            lcd.printout("Watering...                 ")
+            time.sleep(8)
+
+        GPIO.output(pump, GPIO.LOW)
+
+        now = datetime.datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # Print to Display
+        # lcd.setRGB(248,248,60)
+        lcd.setCursor(0, 0)
+        lcd.printout("Last Watered:                ")
+        lcd.setCursor(0, 1)
+        lcd.printout(dt_string + "                 ")
+main()
+
+
+
+
